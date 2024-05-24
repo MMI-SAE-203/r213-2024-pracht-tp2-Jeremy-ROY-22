cec3b53d88c34083a5212001095c43962597ec7d
jeremy roy <jeremy.roy@edu.univ-fcomte.fr>
2024-05-24 11:47:27 +0200

    voila

--- src/pages/offres/index.vue
+++ src/pages/offres/index.vue
@@ -14,9 +14,6 @@ const maisonsFav = await allMaisonsFavori()
 
 <template>
   <h1 class="text-2xl">Bonjour monde ! la liste de toutes les offres du site dans /src/pages/offres/index.vue</h1>
-  <h2>
-    <MaisonCard v-for="uneMaison in maisonsListe" :key="uneMaison.id" v-bind="uneMaison" />
-  </h2>
   <ul>
     <li v-for="uneMaison of maisonsListe" :v-key="uneMaison.id">
       <RouterLink :to="{
@@ -29,4 +26,7 @@ const maisonsFav = await allMaisonsFavori()
       </RouterLink>
     </li>
   </ul>
+  <h2>
+    <MaisonCard v-for="uneMaison in maisonsFav" :key="uneMaison.id" v-bind="uneMaison" />
+  </h2>
 </template>
